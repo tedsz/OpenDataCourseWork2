940aaa99db055f74ef53c67aa60d1d90747e3be2
HuakaiQI <hq1m14@soton.ac.uk>
2016-04-16 14:27:19 +0100

    Class Content

--- OpenDataCourseWork2/src/com/Entity/Match.java
+++ OpenDataCourseWork2/src/com/Entity/Match.java
@@ -1,5 +1,19 @@
 package com.Entity;
 
-public class Match {
+import java.util.Date;
 
+public class Match {
+	//Set properties for match
+	private int matchId; 
+	private String season; //The exact season year of the match
+	private Date date; // The exact holding date of the match
+	private String homeTeam;//The home team name
+	private String awayTeam;//The away team name
+	private int FTHG;// The full time goal of home team
+	private int FTAG;//The full time goal of away team
+	private String fullTimeResult;//The result of full time match; A-Away team win/H-Home team win;
+	private String halfTimeResult;//The result of half time match; A-Away team win/H-Home team win;
+	
+	
+	
 }
